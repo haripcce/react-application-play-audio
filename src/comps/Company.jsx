3dce17c31ad81230b9c5cf52734595ad6a1899db
Harichandra Harmalkar <harichandra.harmalkar@synerzip.com>
2017-11-14 15:42:54 +0530

    optimized for playing

--- src/comps/Company.jsx
+++ src/comps/Company.jsx
@@ -4,6 +4,14 @@ const context = new AudioContext()
 
 export default class Company extends React.Component {
 
+constructor(){
+super();
+this.state = {
+selectedName:null,
+selectedBuffer:null
+};
+}
+
 onCompanyClick(){
 this.props.onCompClick();
 }
@@ -13,21 +21,22 @@ e.stopPropagation();
 }
 onPlay(e){
 
+if(this.state.selectedBuffer !=null){
+  var source = context.createBufferSource(); // creates a sound source
+  source.buffer = this.state.selectedBuffer;                    // tell the source which sound to play
+  source.connect(context.destination);       // connect the source to the context's destination (the speakers)
+  source.start(0);
 
-//if(soundBuffer !=null){
-//  var source = context.createBufferSource(); // creates a sound source
-//  source.buffer = buffer;                    // tell the source which sound to play
-//  source.connect(context.destination);       // connect the source to the context's destination (the speakers)
-//  source.start(0);
-
-//}else{
+}else{
+var parent = this;
+this.setState({selectedName:this.props.name})
 const URL = this.props.url;
 const options = { method: 'GET' }
 fetch(URL, options)
 .then(response => response.arrayBuffer())
 .then(response => {
   context.decodeAudioData(response, buffer => {
-
+    parent.setState({selectedBuffer:buffer})
     var source = context.createBufferSource();
     source.buffer = buffer;
     source.connect(context.destination);
@@ -36,7 +45,7 @@ fetch(URL, options)
 
   });
 })
-//}
+}
 
 e.stopPropagation();
 }
